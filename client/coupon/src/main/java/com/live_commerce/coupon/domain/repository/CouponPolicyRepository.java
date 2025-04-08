892b9c11a368a135e08ea1444ff7339133cce44f
Choi Hojin <jkhh9725@gmail.com>
2025-04-08 22:46:59 +0900

    feat: 쿠폰 정책 생성 기능 구현

--- client/coupon/src/main/java/com/live_commerce/coupon/domain/repository/CouponPolicyRepository.java
+++ client/coupon/src/main/java/com/live_commerce/coupon/domain/repository/CouponPolicyRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponPolicyRepository extends JpaRepository<CouponPolicy, UUID> {
 
+  Boolean existsByName(String name);
 }
--- client/coupon/src/main/java/com/live_commerce/coupon/presentation/controller/CouponPolicyController.java
+++ client/coupon/src/main/java/com/live_commerce/coupon/presentation/controller/CouponPolicyController.java
@@ -1,7 +1,13 @@
 package com.live_commerce.coupon.presentation.controller;
 
 import com.live_commerce.coupon.application.service.CouponPolicyService;
+import com.live_commerce.coupon.infrastructure.common.ResponseUtil;
+import com.live_commerce.coupon.presentation.common.ApiResponse;
+import com.live_commerce.coupon.presentation.dto.request.CreateCouponPolicyRequest;
+import com.live_commerce.coupon.presentation.dto.response.CreateCouponPolicyResponse;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,5 +17,10 @@ public class CouponPolicyController {
 
   private final CouponPolicyService couponService;
 
-
+  @PostMapping("/")
+  public ResponseEntity<ApiResponse<CreateCouponPolicyResponse>> createCoupon(
+      @Valid @RequestBody CreateCouponPolicyRequest request) {
+    CreateCouponPolicyResponse response = couponService.createCouponPolicy(request);
+    return ResponseUtil.success(response);
+  }
 }
