440247647c29e71eed8f2dc78e3a353428b561f0
Choi Hojin <jkhh9725@gmail.com>
2025-04-09 23:37:10 +0900

    feat: IssuedCouponController 생성(KAN-30)

---
+++ client/coupon/src/main/java/com/live_commerce/coupon/presentation/controller/IssuedCouponController.java
@@ -0,0 +1,12 @@
+package com.live_commerce.coupon.presentation.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/issued-coupons")
+public class IssuedCouponController {
+
+
+}
