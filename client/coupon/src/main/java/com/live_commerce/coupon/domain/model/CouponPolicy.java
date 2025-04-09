1ef25f2c62c3b376140c242e606a3185bbb947d2
Choi Hojin <jkhh9725@gmail.com>
2025-04-09 16:40:45 +0900

    refactor: CouponPolicyValidator 생성

--- client/coupon/src/main/java/com/live_commerce/coupon/domain/model/CouponPolicy.java
+++ client/coupon/src/main/java/com/live_commerce/coupon/domain/model/CouponPolicy.java
@@ -5,7 +5,6 @@ import com.live_commerce.coupon.presentation.dto.request.UpdateCouponPolicyReque
 import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Optional;
 import java.util.UUID;
 import lombok.*;
 
@@ -65,23 +64,21 @@ public class CouponPolicy extends BaseEntity {
     validateDiscountType();
   }
 
-  public void updateCouponPolicy(String name, DISCOUNT_TYPE discountType, BigDecimal discountValue,
-      BigDecimal minOrderAmt, BigDecimal maxOrderAmt, LocalDateTime startAt,
-      LocalDateTime endAt, boolean isActive){
+  public void updateCouponPolicy(UpdateCouponPolicyRequest request){
 
     if (name == null || discountType == null || discountValue == null
         || startAt == null || endAt == null) {
       throw new IllegalArgumentException("CouponPolicy의 필수 값이 null일 수 없습니다.");
     }
 
-    this.name = name;
-    this.discountType = discountType;
-    this.discountValue = discountValue;
-    this.minOrderAmt = minOrderAmt ;
-    this.maxOrderAmt = maxOrderAmt;
-    this.startAt = startAt;
-    this.endAt = endAt;
-    this.isActive = isActive;
+    this.name = request.name();
+    this.discountType = request.discountType();
+    this.discountValue = request.discountValue();
+    this.minOrderAmt = request.minOrderAmt();
+    this.maxOrderAmt = request.maxOrderAmt();
+    this.startAt = request.startAt();
+    this.endAt = request.endAt();
+    this.isActive = request.isActive();
 
     validateDiscountType();
   }
