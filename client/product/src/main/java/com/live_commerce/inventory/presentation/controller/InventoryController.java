ec88b8b64e6d69a1a32073cbf4421f9f1d093503
thegreathnb <thegreathnb@kakao.com>
2025-04-10 10:56:56 +0900

    feat: 상품재고 crud 기능 구현

---
+++ client/product/src/main/java/com/live_commerce/inventory/presentation/controller/InventoryController.java
@@ -0,0 +1,8 @@
+package com.live_commerce.inventory.presentation.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InventoryController {
+}
