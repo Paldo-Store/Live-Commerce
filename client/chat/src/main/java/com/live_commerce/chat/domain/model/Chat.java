9c5ad8848279d3cf52d410c8ecf5f0f199ed4535
ahyun <kaeruruk@naver.com>
2025-04-12 19:09:16 +0900

    feat: Chat entity 생성(KAN-37)

---
+++ client/chat/src/main/java/com/live_commerce/chat/domain/model/Chat.java
@@ -0,0 +1,34 @@
+package com.live_commerce.chat.domain.model;
+
+import com.live_commerce.chat.infrastructure.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_chat")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Chat extends BaseEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "chatting", nullable = false)
+    private String chatting;
+
+    public Chat(String userId, String chatting) {
+        this.userId = UUID.fromString(userId);
+        this.chatting = chatting;
+    }
+}
