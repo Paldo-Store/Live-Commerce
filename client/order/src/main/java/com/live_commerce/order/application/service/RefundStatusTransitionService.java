f924b2a96790da757044fe7d180926ff3d468452
ahyun <kaeruruk@naver.com>
2025-04-12 17:04:59 +0900

    feat: PaymentClient의 결제 승인, 취소 요청 생성, 쿠폰 할인 적영된 금액으로 파라미터 변경(KAN-21)

---
+++ client/order/src/main/java/com/live_commerce/order/application/service/RefundStatusTransitionService.java
@@ -0,0 +1,11 @@
+package com.live_commerce.order.application.service;
+
+import com.live_commerce.order.domain.model.Order;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RefundStatusTransitionService {
+    public void markAsRefunded(Order order) {
+
+    }
+}
--- client/order/src/main/java/com/live_commerce/order/infrastructure/client/PaymentClient.java
+++ client/order/src/main/java/com/live_commerce/order/infrastructure/client/PaymentClient.java
@@ -1,6 +1,8 @@
 package com.live_commerce.order.infrastructure.client;
 
+import com.live_commerce.order.infrastructure.client.response.PaymentSuccessResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,7 +11,24 @@ import java.util.UUID;
 @FeignClient(name = "payment")
 public interface PaymentClient {
 
-    //결제 취소 요청을 보냄 (order -> payment) 결제 취소하려는 해당 주문id를 payment로 보내줌
-    @PostMapping("/api/payments/cancel") 
-    void cancelPayment(@RequestParam("orderId") UUID orderId);
+    /**
+     * 결제 승인 요청
+     * @param orderId 주문 ID
+     * @param finalPaidPrice 최종 결제 금액 (쿠폰 적용 후 금액)
+     * @return 결제 성공 응답
+     */
+    @PostMapping("/api/v1/payments/{orderId}/approve")
+    PaymentSuccessResponse approvePayment(@PathVariable("orderId") UUID orderId,
+                                          @RequestParam("finalPaidPrice") Long finalPaidPrice);
+
+    /**
+     * 결제 취소 요청
+     * @param orderId 주문 ID
+     * @param finalPaidPrice 최종 결제 금액 (쿠폰 적용 후 금액)
+     * @return 결제 취소 응답
+     */
+    // 결제 취소 요청을 보냄 (order -> payment) 결제 취소하려는 해당 주문id와 최종 결제 금액을 payment로 보내줌
+    @PostMapping("/api/v1/payments/{orderId}/cancel")
+    void cancelPayment(@PathVariable("orderId") UUID orderId,
+                       @RequestParam("finalPaidPrice") Long finalPaidPrice);
 }
