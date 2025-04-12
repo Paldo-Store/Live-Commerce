5dfac3c2c729728d0e46fbff54b5b196919fda3b
STW5 <xodnd8384@gmail.com>
2025-04-12 22:33:40 +0900

    feat: ai 분석 삭제 기능 구현 및 권한 처리 (KAN-35)

--- client/ai/src/main/java/com/live_commerce/ai/presentation/controller/AiController.java
+++ client/ai/src/main/java/com/live_commerce/ai/presentation/controller/AiController.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import com.live_commerce.ai.application.dto.request.AiRequestDto;
@@ -13,6 +15,7 @@ import com.live_commerce.ai.application.dto.response.AiCreateResponseDto;
 import com.live_commerce.ai.application.dto.response.AiGetResponseDto;
 import com.live_commerce.ai.application.service.AiService;
 import com.live_commerce.ai.infrastructure.common.ResponseUtil;
+import com.live_commerce.ai.infrastructure.security.RequestUserDetails;
 import com.live_commerce.ai.presentation.common.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
@@ -32,20 +35,36 @@ public class AiController {
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<ApiResponse<AiGetResponseDto>> getAiAnalysis(@PathVariable UUID id) {
-		AiGetResponseDto response = aiService.getAiAnalysis(id);
-
+	@PreAuthorize("hasRole('MASTER')")
+	public ResponseEntity<ApiResponse<AiGetResponseDto>> getAiAnalysis(
+		@PathVariable UUID id,
+		@AuthenticationPrincipal RequestUserDetails requestUserDetails
+	) {
+		AiGetResponseDto response = aiService.getAiAnalysis(id, requestUserDetails);
 		return ResponseUtil.success(response);
 	}
 
 	@GetMapping("/search")
+	@PreAuthorize("hasRole('MASTER')")
 	public ResponseEntity<ApiResponse<Page<AiGetResponseDto>>> getAiAnalysisList(
-		@ModelAttribute AiSearchCondition condition, Pageable pageable) {
-
-		Page<AiGetResponseDto> response = aiService.getAiAnalysisList(condition, pageable);
+		@ModelAttribute AiSearchCondition condition,
+		Pageable pageable,
+		@AuthenticationPrincipal RequestUserDetails requestUserDetails
+	) {
+		Page<AiGetResponseDto> response = aiService.getAiAnalysisList(condition, pageable, requestUserDetails);
 		return ResponseUtil.success(response);
 	}
 
+
+	@DeleteMapping("/{id}")
+	@PreAuthorize("hasRole('MASTER')")
+	public ResponseEntity<ApiResponse<Void>> deleteAiAnalysis(
+		@PathVariable UUID id,
+		@AuthenticationPrincipal RequestUserDetails requestUserDetails
+	) {
+		aiService.deleteAiAnalysis(id, requestUserDetails);
+		return ResponseUtil.noContent();
+	}
 
 
 }
