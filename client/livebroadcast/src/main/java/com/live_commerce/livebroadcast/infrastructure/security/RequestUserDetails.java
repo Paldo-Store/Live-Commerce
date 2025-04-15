a3c32e18a3416c9f7dc9e70c84e1b1259787d1bf
thegreathnb <thegreathnb@kakao.com>
2025-04-15 22:32:04 +0900

    chore: 시큐리티 설정 적용

---
+++ client/livebroadcast/src/main/java/com/live_commerce/livebroadcast/infrastructure/security/RequestUserDetails.java
@@ -0,0 +1,41 @@
+package com.live_commerce.livebroadcast.infrastructure.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.UUID;
+
+
+public class RequestUserDetails implements UserDetails {
+
+	private final UUID userId;
+	private final String username;
+	private final Collection<? extends GrantedAuthority> authorities;
+
+	public RequestUserDetails(UUID userId, String username, Collection<? extends GrantedAuthority> authorities) {
+		this.userId = userId;
+		this.username = username;
+		this.authorities = authorities;
+	}
+
+	public UUID getUserId() {
+		return userId;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getUsername() {
+		return username;
+	}
+
+	@Override public String getPassword() { return null; }
+	@Override public boolean isAccountNonExpired() { return true; }
+	@Override public boolean isAccountNonLocked() { return true; }
+	@Override public boolean isCredentialsNonExpired() { return true; }
+	@Override public boolean isEnabled() { return true; }
+}
