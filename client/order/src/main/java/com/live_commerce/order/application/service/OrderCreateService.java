8b45bc0d6bfeb7c5bc6a54f7b0c7dbc5e05d5fb7
ahyun <kaeruruk@naver.com>
2025-04-16 15:44:08 +0900

    refactor: order create 로직 수정 - product와 통신(KAN-21)

--- client/order/src/main/java/com/live_commerce/order/application/service/OrderCreateService.java
+++ client/order/src/main/java/com/live_commerce/order/application/service/OrderCreateService.java
@@ -61,35 +61,27 @@ public class OrderCreateService {
         }
         log.info("상품 조회 완료");
 
-        // 재고가 주문보다 많나 확인 - 주문이 현재 가능한 상태인지 확인 로직
-        // 2. 상품 재고 존재 여부 확인 로직
+        // 2. 재고가 주문보다 많나 체크 - 주문이 현재 가능한 상태인지 확인 로직
         ApiResponse<InventoryCheckResponseDto> responseInventory = inventoryClient.checkOrderableInventory(request.productId(), request.productQuantity());
         InventoryCheckResponseDto checkInventory = responseInventory.getData();
         if (!checkInventory.orderAvailable()) {
             throw new OrderException("재고가 없는 상태입니다.", HttpStatus.BAD_REQUEST);
         }
-
         log.info("재고 존재 여부 확인 완료");
 
-        //TODO inventory 모듈에서
-        // 1. productId 조회 후 상품 정보 가져오기
-        // 2. 상품 재고 0개인지 확인
-        // 3. 주문개수 < 재고개수 체크
-        // 4. 토탈 주문 금액 계산
+        // 해당 상품의 남은 재고 수량 들고오기
+//        ApiResponse<InventoryCheckQuantityResponseDto> responseInventoryByQuantity = inventoryClient.checkInventoryQuantity(request.productId(), request.productQuantity());
+//        InventoryCheckQuantityResponseDto getInventoryQuantity = responseInventoryByQuantity.getData();
 
-        // 3. 해당 상품의 남은 재고 수량 들고오기
-        ApiResponse<InventoryCheckQuantityResponseDto> responseInventoryByQuantity = inventoryClient.checkInventoryQuantity(request.productId(), request.productQuantity());
-        InventoryCheckQuantityResponseDto getInventoryQuantity = responseInventoryByQuantity.getData();
-
-        int stock = getInventoryQuantity.availableQuantity(); // 실제 상품의 해당 재고 수량 (응답 product-> order)
+        //int stock = getInventoryQuantity.availableQuantity(); // 실제 상품의 해당 재고 수량 (응답 product-> order)
         int orderQty = request.productQuantity(); // 사용자가 주문한 상품의 수량 (요청 order -> product)
         log.info("재고 수량 들고오기");
 
         // 4. 상품의 재고 수량이 0이거나, or 주문한 상품 개수 > 상품 재고 개수
-        if ( (stock == 0) || (orderQty > stock)) {
-            throw new OrderException("해당 상품이 존재하지 않거나 재고가 충분하지 않습니다.", HttpStatus.BAD_REQUEST);
-        }
-        log.info("재고 > 주문 수량");
+//        if ( (stock == 0) || (orderQty > stock)) {
+//            throw new OrderException("해당 상품이 존재하지 않거나 재고가 충분하지 않습니다.", HttpStatus.BAD_REQUEST);
+//        }
+        //log.info("재고 > 주문 수량");
 
         // 5. total 주문 금액 계산
         // 주문한 수량 * 주문한 상품 한 개의 가격
