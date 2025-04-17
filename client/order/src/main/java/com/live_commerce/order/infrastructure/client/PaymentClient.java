88106c2119cf2079142742b44ce2fc5bd13ddd0d
ahyun <kaeruruk@naver.com>
2025-04-17 17:52:48 +0900

    refactor: order create에서 payment client 수정(KAN-21)

--- client/order/src/main/java/com/live_commerce/order/infrastructure/client/PaymentClient.java
+++ client/order/src/main/java/com/live_commerce/order/infrastructure/client/PaymentClient.java
@@ -1,18 +1,15 @@
 package com.live_commerce.order.infrastructure.client;
 
 import com.live_commerce.order.infrastructure.PaymentReadyResponseDto;
-import com.live_commerce.order.infrastructure.client.response.PaymentSuccessResponse;
 import com.live_commerce.order.presentation.common.ApiResponse;
-import com.live_commerce.payment.application.dto.request.PaymentRefundResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.UUID;
 
-@FeignClient(name = "payment")
+@FeignClient(name = "payment" , url = "http://localhost:19091", path = "/api/v1/payments")
 public interface PaymentClient {
 
     //결제 대기로 요청 전송
--- client/order/src/main/java/com/live_commerce/order/infrastructure/client/PaymentRefundResponseDto.java
+++ client/order/src/main/java/com/live_commerce/order/infrastructure/client/PaymentRefundResponseDto.java
@@ -1,9 +1,9 @@
-package com.live_commerce.order.infrastructure;
+package com.live_commerce.order.infrastructure.client;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
-public record PaymentRefundResponseDTO(
+public record PaymentRefundResponseDto(
         UUID paymentId,
         UUID orderId,
         String status,
