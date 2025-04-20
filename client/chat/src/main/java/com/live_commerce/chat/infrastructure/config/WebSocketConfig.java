91caf0e215f7c33a971a811f4323c53dd60251f2
ahyun <kaeruruk@naver.com>
2025-04-21 01:47:14 +0900

    refactor: websocket 로직 수정(KAN-43)

--- client/chat/src/main/java/com/live_commerce/chat/infrastructure/config/WebSocketConfig.java
+++ client/chat/src/main/java/com/live_commerce/chat/infrastructure/config/WebSocketConfig.java
@@ -1,25 +1,45 @@
 package com.live_commerce.chat.infrastructure.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer  {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // 클라이언트에게 메시지를 전송할 대역을 설정
-        registry.enableSimpleBroker("/topic");  // /topic/broadcast/{broadcastId}로 구독하는 클라이언트에게 메시지 전송
-        registry.setApplicationDestinationPrefixes("/app");  // 클라이언트가 보낼 메시지의 prefix 설정
+    private final WebSocketHandler webSocketHandler;
+
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//        // 클라이언트에게 메시지를 전송할 대역을 설정
+//        registry.enableSimpleBroker("/topic");  // /topic/broadcast/{broadcastId}로 구독하는 클라이언트에게 메시지 전송
+//        registry.setApplicationDestinationPrefixes("/app");  // 클라이언트가 보낼 메시지의 prefix 설정
+//    }
+//
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        // WebSocket의 엔드포인트 설정
+//        registry.addEndpoint("/ws")
+//                .setAllowedOriginPatterns("*")
+//                .withSockJS();  // /ws가 클라이언트의 WebSocket 연결 엔드포인트
+//    }
+
+    @Bean
+    public WebSocketHandler webSocketHandler() {
+        return new WebSocketHandler();
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // WebSocket의 엔드포인트 설정
-        registry.addEndpoint("/ws").withSockJS();  // /ws가 클라이언트의 WebSocket 연결 엔드포인트
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry
+                // /ws/conn 경로로 WebSocket 연결을 허용
+                .addHandler(webSocketHandler, "/ws/conn")
+                // CORS 허용
+                .setAllowedOrigins("*");
     }
 }
---
+++ client/chat/src/main/java/com/live_commerce/chat/infrastructure/config/WebSocketHandler.java
@@ -0,0 +1,47 @@
+package com.live_commerce.chat.infrastructure.config;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.*;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    // 소켓 세션을 저장할 Set
+    private static final Set<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
+
+    // 소켓 연결 확인
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("{} 연결됨", session.getId());
+        sessions.add(session);
+        session.sendMessage(new TextMessage("WebSocket 연결 완료"));
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        sessions.remove(session);
+    }
+
+    public void broadcast(String message) {
+        for (WebSocketSession session : sessions) {
+            if (session.isOpen()) {
+                try {
+                    session.sendMessage(new TextMessage(message));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+}
