f82faf7f92fcc05c50e88d5e15baae0e496f07e2
STW5 <xodnd8384@gmail.com>
2025-04-29 14:45:32 +0900

    refactor: 첫가입쿠폰 이벤트 DTO 이름 변경 (KAN-85)

--- client/user/src/main/java/com/live_commerce/user/infrastructure/kafka/event/FirstJoinCouponEvent.java
+++ client/user/src/main/java/com/live_commerce/user/infrastructure/kafka/event/FirstJoinCouponEvent.java
@@ -2,7 +2,7 @@ package com.live_commerce.user.infrastructure.kafka.event;
 
 import java.util.UUID;
 
-public record FirstJoinCouponMessage(
+public record FirstJoinCouponEvent(
 	UUID userId
 ) {
 }
