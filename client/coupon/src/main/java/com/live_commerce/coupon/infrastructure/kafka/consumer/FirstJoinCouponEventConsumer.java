c848d3116fef46e424202009008e91b400009fb2
Choi Hojin <jkhh9725@gmail.com>
2025-04-29 15:29:43 +0900

    chore: kafka 클래스 명 수정 및 설정 이동

--- client/coupon/src/main/java/com/live_commerce/coupon/infrastructure/kafka/consumer/FirstJoinCouponEventConsumer.java
+++ client/coupon/src/main/java/com/live_commerce/coupon/infrastructure/kafka/consumer/FirstJoinCouponEventConsumer.java
@@ -1,8 +1,7 @@
 package com.live_commerce.coupon.infrastructure.kafka.consumer;
 
 import com.live_commerce.coupon.application.service.IssuedCouponService;
-import com.live_commerce.coupon.infrastructure.config.KafkaConfig;
-import com.live_commerce.coupon.infrastructure.kafka.dto.FirstJoinCouponMessage;
+import com.live_commerce.coupon.infrastructure.kafka.event.FirstJoinCouponEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -11,16 +10,15 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class IssuedCouponConsumer {
+public class FirstJoinCouponEventConsumer {
 
   private final IssuedCouponService issuedCouponService;
 
   @KafkaListener(
-      topics = KafkaConfig.FIRST_COUPON_TOPIC,
-      groupId = "coupon-first-join-group",
-      containerFactory = "firstJoinListenerContainerFactory"
+      topics = "first-join-coupon",
+      groupId = "${spring.application.name}"
   )
-  public void onFirstJoin(FirstJoinCouponMessage msg) {
+  public void onFirstJoin(FirstJoinCouponEvent msg) {
     issuedCouponService.issueFirstCouponDirectly(msg.userId());
     log.info("✅ 회원가입 쿠폰이 정상 발급되었습니다.(kafka): userId={}", msg.userId());
 
