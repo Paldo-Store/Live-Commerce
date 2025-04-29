cde51529703a2a400cb3f1ca4a81b0f42bd14a11
ahyun <kaeruruk@naver.com>
2025-04-29 21:40:57 +0900

    feat: order-payment 결제 응답 처리 성공(KAN-87)

---
+++ client/order/src/main/java/com/live_commerce/order/kafkaOrder/payment/PaymentEventConsumer.java
@@ -0,0 +1,23 @@
+package com.live_commerce.order.kafkaOrder.payment;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PaymentEventConsumer {
+
+    private static final String COMPLETED_TOPIC = "payment-completed";
+
+    @KafkaListener(
+            topics = COMPLETED_TOPIC,
+            groupId = "order"
+    )
+    public void listenPaymentCompleted(PaymentCompletedEvent msg) {
+        log.info("✅ 결제 성공 이벤트 수신(kafka): orderId={}, message={}", msg.orderId(), msg.message());
+    }
+}
