d0d652310297148a02094b0d4dce210ebe8de7e8
ming <hnbnge@gmail.com>
2026-05-07 14:30:11 +0900

    feat: InventoryEventConsumer Outbox 패턴 적용 및 보상 트랜잭션 연결

--- product/src/main/java/com/live_commerce/product/ProductApplication.java
+++ product/src/main/java/com/live_commerce/product/ProductApplication.java
@@ -3,8 +3,10 @@ package com.live_commerce.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients
+@EnableScheduling
 @SpringBootApplication
 public class ProductApplication {
 
